86d8f0fd35d902cd4c332f08d4a9d470390d3f30
i230063 <luanamral84@gmail.com>
2025-10-30 12:23:19 +0000

    Alteração Dia 30/10

--- TaskToDo/Form1.cs
+++ TaskToDo/Form1.cs
@@ -16,5 +16,10 @@ namespace TaskToDo
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int idade;
+        }
     }
 }
